3efb9806ad590a903e08929f35bd96b86c0fa992
Epidemy <aipdimi@gmail.com>
2022-08-11 14:39:49 +0430

    Shell script start added

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export API_ID=""
+export API_HASH=""
+export STRING_SESSION=""
+export FORWARD_AS_COPY=True
+export SOURCE_CHAT_ID=""
+export DESTINATION_CHAT_ID=""
+
+alias venv="source $HOME/TelegramContentFilter/venv/bin/activate"
+nohup python3 $HOME/TelegramContentFilter/main.py &
