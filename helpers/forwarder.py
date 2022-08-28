3dbe4a35104bc4e112a1643052e7c3bc973f71a7
Epidemy <aipdimi@gmail.com>
2022-08-28 19:41:56 +0430

    Forward message that contail a specific regex

--- helpers/forwarder.py
+++ helpers/forwarder.py
@@ -1,6 +1,7 @@
-from random import randint
-import asyncio
+from asyncio import sleep
 import logging
+from random import randint
+from re import search
 from configs import Config
 from pyrogram import Client
 from pyrogram.errors import FloodWait
@@ -16,22 +17,23 @@ async def ForwardMessage(client: Client, message_id):
             return
 
         logger.warn("Check forwarding.")
+        could_forward_pattern = "(?s).*Profit:\s\d+\.?\d*(?s).*"
 
         is_exist = await client.get_messages(Config.FORWARD_FROM_CHAT_ID, message_id)
-        if not is_exist.text:
+        if not is_exist.text or not search(could_forward_pattern, is_exist.text):
             logger.warn("No message found for forwarding.")
             return
 
         # Set a delay
-        await asyncio.sleep(randint(10, 20))
+        await sleep(randint(10, 30))
         sent = await client.forward_messages(chat_id=Config.FORWARD_TO_CHAT_ID, from_chat_id=Config.FORWARD_FROM_CHAT_ID, message_ids=message_id)
         if sent:
             logger.warn("Message Forwarded.")
 
     except FloodWait as e:
-        await asyncio.sleep(e.x)
+        await sleep(e.x)
         await client.send_message(chat_id="me", text=f"#FloodWait: Stopped Forwarder for `{e.x}s`!")
-        await asyncio.sleep(Config.SLEEP_TIME)
+        await sleep(Config.SLEEP_TIME)
         await ForwardMessage(client, message_id)
     except Exception as err:
         await client.send_message(chat_id="me", text=f"#ERROR: `{err}`\n\nUnable to Forward Message to `{str(Config.DESTINATION_CHAT_ID)}`")
